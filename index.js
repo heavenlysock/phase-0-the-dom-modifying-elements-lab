ca612722c17e7b34c65de8be0916013d84a11cf9
bobertsworld <bobertsworld@gmail.com>
2022-11-02 16:28:36 -0700

    commit tree 9e05dc8ef9fe47b7cecf32c79f4daeb2d74f6bd7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+const main = document.querySelector("#main")   
+console.log(main)
+main.remove()
+
+
+
+// const newHeader = document.createElement('h1')
+// newHeader.id = "victory"
+// // newHeader.setAttribute("id", "victory")
+// console.log(newHeader)
+
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = 'Matt is the champion'
+console.log(newHeader)
+console.log(newHeader.textContent)
+
+
+const div = document.querySelector('append-to-this')
+
+div.append(newHeader)
